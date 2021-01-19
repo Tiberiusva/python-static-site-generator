671ce535b1a55523f8b63729c25981bc47c881bd
Craig Morris <Craig.Morris@macksonconsulting.com>
2021-01-19 11:08:48 -0500

    fix errors

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,4 @@
-from pathlib import path 
+from pathlib import Path 
 
 class Site:
     def __init__(self,source,dest):
@@ -7,7 +7,7 @@ class Site:
 
     def create_dir(self,path):
         directory = self.dest / path.relative_to(self.source)
-        diretory.mkdir(parents=True,exist_ok=True)
+        directory.mkdir(parents=True,exist_ok=True)
     
     def build(self):
          self.dest.mkdir(parents=True,exist_ok=True)
