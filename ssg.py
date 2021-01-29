76bbe7d2b4b84fcfb74c6cbd951cc325841f6540
Craig Morris <Craig.Morris@macksonconsulting.com>
2021-01-29 08:40:16 -0500

    pep8

--- ssg.py
+++ ssg.py
@@ -2,13 +2,13 @@ import typer
 from ssg.site import Site
 import ssg.parsers
 
-def main (source="content",dest="dist"):
-    parsers=[ssg.parsers.ResourceParser(),ssg.parsers.MarkdownParser(),ssg.parsers.ReStructuredTextParser()]
+
+def main(source="content", dest="dist"):
+    parsers = [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]
     for p in parsers:
         print(p.__repr__)
-    config = {'source':source,'dest':dest,'parsers':parsers}
+    config = {'source': source, 'dest': dest, 'parsers': parsers}
     Site(**config).build()
-     
-    
+
 
 typer.run(main)
--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,57 +10,64 @@ from ssg.auto_repr import *
 
 @auto_repr
 class Parser:
-    
-    extensions:List[str]=[]
-    def valid_extension(self,extension):
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self,path:Path,source:Path,dest:Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
     @property
     def path(self):
         return self.path
+
     @property
     def source(self):
         return self.source
+
     @property
     def dest(self):
         return self.dest
 
-    def read(self,path):
-        with open(path,"r") as file:
+    def read(self, path):
+        with open(path, "r") as file:
             return file.read()
-    def write(self,path,dest,content,ext=".html"):
-        full_path= dest / path.with_suffix(ext).name
-        with open(full_path,"w") as file:
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
             file.write(content)
 
-    def copy(self,path, source, dest):
-        shutil.copy2(path,dest / path.relative_to(source))
-    
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
 
 @auto_repr
 class ResourceParser(Parser):
-    extensions = [".jpg",".png",".gif",".css",".html"]
-    def parse(self,path,source,dest):
-        self.copy(path,source,dest)
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
+
 
 @auto_repr
 class MarkdownParser(Parser):
-    extensions:List[str]=[".md",".markdown"]
+    extensions: List[str] = [".md", ".markdown"]
 
-    def parse(self,path:Path,source:Path,dest:Path):
-        content=Content.load(self.read(path))
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path,dest,html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format( path.name, content))
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
-@auto_repr   
+
+@auto_repr
 class ReStructuredTextParser(Parser):
-    extensions:List[str]=[".rst"]
-    def parse(self,path:Path,source:Path,dest:Path):
-        content=Content.load(self.read(path))
-        html=publish_parts(content.body,writer_name="html5")
-        self.write(path,dest,html["html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+    extensions: List[str] = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
